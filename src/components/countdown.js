c56a2bdf1c1d1153c11c57b5c88447f9b5611cf5
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-02-26 23:41:48 -0330

    Countdown refined

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,30 +1,57 @@
-// Set the date we're counting down to
 var countDownDate = new Date("Aug 8, 2026 00:00:00").getTime();
 // var countDownDate = new Date("Feb 26, 2025 21:05:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+    var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    var difference = countDownDate - now;
 
-  // Time calculations for weeks, days, hours, minutes and seconds
-  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7))
-  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var weeks = Math.floor(difference / (1000 * 60 * 60 * 24 * 7))
+    var days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="countdown"
-  document.getElementById("countdown").innerHTML = "ETA: "+ weeks + " WEEKS " + days + " DAYS " + hours + " HOURS "
-  + minutes + " MINUTES " + seconds + " SECONDS ";
+    // Make this look better
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "COMPETITION DAY";
-  }
-}, 1000);
+    if (weeks == 1) {
+        var w = " WEEK "
+    } else {
+        var w = " WEEKS "
+    }
+
+    if (days == 1) {
+        var d = " DAY "
+    } else {
+        var d = " DAYS "
+    }
+
+    if (hours == 1) {
+        var h = " HOUR "
+    } else {
+        var h = " HOURS "
+    }
+
+    if (minutes == 1) {
+        var m = " MINUTE "
+    } else {
+        var m = " MINUTES "
+    }
+
+    if (seconds == 1) {
+        var s = " SECOND "
+    } else {
+        var s = " SECONDS "
+    }
+    // Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = "ETA: "+ weeks + w + days + d + hours + h
+    + minutes + m + seconds + s;
+
+    // If the count down is finished, write some text
+    if (difference < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "COMPETITION DAY";
+    }
+}, 100);
