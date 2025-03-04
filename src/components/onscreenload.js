adbf36154ab02d7369258fa38fe92038130d0ce7
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-03-04 14:53:34 -0330

    Fixed responsive issue

--- src/components/onscreenload.js
+++ src/components/onscreenload.js
@@ -1,15 +1,24 @@
-const intersectionCallback = (entries) => {
-    for (const entry of entries) { 
-      if (entry.isIntersecting) { 
-        entry.target.classList.add('show'); 
-        }
-    }   
-}
-  
 
-const observer = new IntersectionObserver(intersectionCallback);
 
-const items = document.querySelectorAll('.banner');
-for (const item of items) {
-    observer.observe(item);
-}
+// const things = document.querySelectorAll('.banner');
+// for (const thing of things) {
+//     observer.observe(thing);
+// }
+
+var x = setInterval(function() {
+    const intersectionCallback = (entries) => {
+        for (const entry of entries) { 
+          if (entry.isIntersecting) { 
+            entry.target.classList.add('show'); 
+            }
+        }   
+    }
+      
+    
+    const observer = new IntersectionObserver(intersectionCallback);
+    
+    const items = document.querySelectorAll('.blur-load.loaded');
+    for (const item of items) {
+        observer.observe(item);
+    }
+}, 50)
