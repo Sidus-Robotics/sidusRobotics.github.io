a17afaf9517d0f024490c3c0d36117097c919219
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-03-09 22:34:09 -0230

    Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://sidus-robotics.github.io/sidusrobotics.github.io/', //PUT LINK HERE
     devToolbar: {
       enabled: false
     }
